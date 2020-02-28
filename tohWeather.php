cad9a2e7f5e5e7c4c4598d1c98c7eb657fc8126c
tuantt <tuantt@tohsoft.com>
2020-02-28 16:14:13 +0700

    css

--- tohWeather.php
+++ tohWeather.php
@@ -1,5 +1,5 @@
 ﻿<?php 
-    $arr = ['com.wftab.weather.forecast', 'com.wftab.weather.forecast.pro'];
+    $arr = ['com.wftab.weather.forecast', 'com.wftab.weather.forecast.pro', 'com.tohsoft.weather.sunrise.sunset.gen2', 'com.tohsoft.weather.sunrise.sunset.gen2.pro'];
     if(isset($_GET["application_id"]) && in_array($_GET["application_id"], $arr)){
         $url = "http://45.79.69.97/index2.php?";
         foreach($_GET as $key => $value){
