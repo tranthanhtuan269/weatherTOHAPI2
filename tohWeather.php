6f7a14ef6de61a9b5dae731164ee1fffe4a42717
tuantt <tuantt@tohsoft.com>
2020-03-04 09:19:56 +0700

    css

--- tohWeather.php
+++ tohWeather.php
@@ -1,7 +1,8 @@
 ﻿<?php 
     $arr = ['com.wftab.weather.forecast', 'com.wftab.weather.forecast.pro', 'com.tohsoft.weather.sunrise.sunset.gen2', 'com.tohsoft.weather.sunrise.sunset.gen2.pro'];
-    if(isset($_GET["application_id"]) && in_array($_GET["application_id"], $arr)){
-        $url = "http://45.79.69.97/index2.php?";
+    // if(isset($_GET["application_id"]) && in_array($_GET["application_id"], $arr)){
+    if(true){
+        $url = "https://goweatherradar.com/index2.php?";
         foreach($_GET as $key => $value){
             $url .= $key . '=' . $value . '&';
         }
